65473496b4ff5e6f0cdb228b87249815770399c4
WN-Shoxy <99392393+WN-Shoxy@users.noreply.github.com>
2023-01-29 23:08:25 +0100

    Add files via upload

---
+++ js/favoris.js
@@ -0,0 +1,30 @@
+let afficherBandeau = document.querySelector('.bandeau-info');
+let infoRecup = JSON.parse(localStorage.getItem('favoris'));
+
+function saveInfo() {
+    for (i = 0; i < infoRecup.length; i++) {
+        afficherBandeau.innerHTML +=`
+        <div class="bandeau-info">
+            <div class="image-resto">
+            <img src="../img/default.jpg" alt="">
+            </div>
+            <div class="description">
+            <h2>${infoRecup[i].titre}</h2>
+            <p>${infoRecup[i].contact}</p>
+            <p>${infoRecup[i].infos}</p>
+            </div>
+            <div class="btns">
+                <i id="croix" class="fas fa-times"></i>
+            </div>
+        </div>`;
+    }
+}
+
+afficherBandeau.onclick = (event) => {
+    let target = event.target;
+    if (target.id === "croix") {
+        target.parentElement.parentElement.remove();
+    }
+}
+
+saveInfo();
